dd081787121c9bffb460019e7dfda8cdc1db8d6e
sdkks <sdkks@users.noreply.github.com>
2026-04-25 16:21:43 +0800

    mdviewer 2.0.3

---
+++ Casks/mdviewer.rb
@@ -0,0 +1,22 @@
+cask "mdviewer" do
+  version "2.0.3"
+  sha256 ""
+
+  url "https://github.com/sdkks/mdviewer/releases/download/v#{version}/MDViewer-#{version}.zip"
+  name "MDViewer"
+  desc "Minimal native macOS Markdown viewer"
+  homepage "https://github.com/sdkks/mdviewer"
+
+  app "MDViewer.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/MDViewer.app"],
+                   sudo: false
+  end
+
+  zap trash: [
+    "~/Library/Preferences/com.torstenmahr.MDViewer.plist",
+    "~/Library/Application Support/MDViewer",
+  ]
+end
