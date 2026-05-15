ae0bea7c0d21d0ee44f35232339841931bff1d3e
Said Akkas <sdkks@users.noreply.github.com>
2026-05-16 00:52:32 +0800

    termin8r 0.5.1

--- Formula/termin8r.rb
+++ Formula/termin8r.rb
@@ -1,9 +1,9 @@
 class Termin8r < Formula
   desc "Headless virtual terminal for AI agents"
   homepage "https://github.com/sdkks/termin8r"
-  url "https://github.com/sdkks/termin8r/releases/download/v0.4.1/termin8r_macos_aarch64"
-  version "0.4.1"
-  sha256 "7c53fbcc0d73ca2ab6d867ac87a95bdd9d1573dbfe608c2ea16b068f44d07aa3"
+  url "https://github.com/sdkks/termin8r/releases/download/v0.5.1/termin8r_macos_aarch64"
+  version "0.5.1"
+  sha256 "5ea2a908bdecaa069ace54b3bf2d9b79356d5d937537c4a589aaddce66c32065"
 
   def install
     bin.install "termin8r_macos_aarch64" => "termin8r"
