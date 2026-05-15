29f2f6b3d8366bec18ea2c05d8659105e62f1dd6
Said Akkas <sdkks@users.noreply.github.com>
2026-05-15 22:18:06 +0800

    termin8r 0.2.1

--- Formula/termin8r.rb
+++ Formula/termin8r.rb
@@ -3,7 +3,7 @@ class Termin8r < Formula
   homepage "https://github.com/sdkks/termin8r"
   url "https://github.com/sdkks/termin8r/releases/download/v0.2.1/termin8r_macos_aarch64"
   version "0.2.1"
-  sha256 ""
+  sha256 "9e5deadc59d13ecea77ab20fafad4f2e5aef0b414560a23552a0e3d56c275ab5"
 
   def install
     bin.install "termin8r_macos_aarch64" => "termin8r"
