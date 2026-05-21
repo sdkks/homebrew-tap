7a00cb908d0d34af6c52f54b63a3211656829152
Said Akkas <sdkks@users.noreply.github.com>
2026-05-21 19:42:35 +0800

    termin8r 0.5.6

--- Formula/termin8r.rb
+++ Formula/termin8r.rb
@@ -1,19 +1,22 @@
 class Termin8r < Formula
   desc "Headless virtual terminal for AI agents"
   homepage "https://github.com/sdkks/termin8r"
-  url "https://github.com/sdkks/termin8r/releases/download/v0.5.5/termin8r_macos_aarch64"
-  version "0.5.5"
-  sha256 "e2be96cc5ff0d4918affc9e3e46f014993f29c7261073030a56c91c92a9a65fe"
+  url "https://github.com/sdkks/termin8r/releases/download/v0.5.6/termin8r_macos_aarch64"
+  version "0.5.6"
+  sha256 "c077ab373b90d41ddefa6eaa893d2d716d7e12379048a294e1884cbd6ea7bd29"
 
   def install
     bin.install "termin8r_macos_aarch64" => "termin8r"
   end
 
-  def post_install
-    system "xattr", "-dr", "com.apple.quarantine", "#{bin}/termin8r"
+  def caveats
+    <<~EOS
+      If macOS prevents this binary from running due to Gatekeeper, clear the quarantine flag manually:
+        xattr -dr com.apple.quarantine #{opt_bin}/termin8r
+    EOS
   end
 
   test do
-    system "#{bin}/termin8r", "--version"
+    assert_match "0.5.6", shell_output("#{bin}/termin8r --version")
   end
 end
