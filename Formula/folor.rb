226e9a3dbe8ca1b8286489155b861587e8933e1a
Said Akkas <sdkks@users.noreply.github.com>
2026-06-04 00:54:48 +0800

    folor 0.3.0

--- Formula/folor.rb
+++ Formula/folor.rb
@@ -1,9 +1,9 @@
 class Folor < Formula
   desc "Recursive tail -f with glob pattern matching"
   homepage "https://github.com/sdkks/folor"
-  url "https://github.com/sdkks/folor/releases/download/v0.2.2/folor-v0.2.2-aarch64-apple-darwin.tar.gz"
-  version "0.2.2"
-  sha256 "dfac80f49b4f83b87b97f9f76d0f9022d09179cb272d5c2b8074916c0df2a1bd"
+  url "https://github.com/sdkks/folor/releases/download/v0.3.0/folor-v0.3.0-aarch64-apple-darwin.tar.gz"
+  version "0.3.0"
+  sha256 "ce3fd617f6bb7e4378c03b94fa132f89fd5129e762d62eec3c1588e4addbd11e"
 
   def install
     bin.install "folor"
@@ -17,6 +17,6 @@ class Folor < Formula
   end
 
   test do
-    assert_match "0.2.2", shell_output("#{bin}/folor --version")
+    assert_match "0.3.0", shell_output("#{bin}/folor --version")
   end
 end
